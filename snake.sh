4725990ede4ef8a20f2e8133addfabbada5fa179
amitsingh29 <amitsinghbhakuni29@gmail.com>
2020-02-17 09:33:35 +0530

    [Amit] Refactor. Adding snake board

--- snake.sh
+++ snake.sh
@@ -1,1 +1,45 @@
 #!/bin/bash
+
+board()
+{
+n=101
+declare -A array
+for(( i=0;i<10;i++ ))
+do
+if(( $i%2==0 ))
+then
+for((j=0;j<10;j++))
+do
+n=$(($n-1))
+array[$i,$j]=$n
+done
+else
+for((j=9;j>=0;j--))
+do
+n=$(($n-1))
+array[$i,$j]=$n
+done
+fi
+done
+echo "________________________________________________________"
+for((i=0;i<10;i++))
+do
+if(( $i==9 ))
+then
+for(( j=0; j<10; j++ ))
+do
+echo -n "| "
+echo -n "${array[$i,$j]} |"
+done
+else
+for(( j=0; j<10; j++ ))
+do
+echo -n "| "
+echo -n "${array[$i,$j]} |"
+done
+fi
+echo ""
+echo "_________________________________________________________"
+done
+}
+board
